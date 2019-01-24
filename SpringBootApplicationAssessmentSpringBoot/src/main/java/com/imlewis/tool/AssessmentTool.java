3b72f08c3b7a577ad896979f24efd9ee5820f159
chandranaveen79 <46917012+chandranaveen79@users.noreply.github.com>
2019-01-24 11:20:48 +0000

    RGB Changes improved

--- SpringBootApplicationAssessmentSpringBoot/src/main/java/com/imlewis/tool/AssessmentTool.java
+++ SpringBootApplicationAssessmentSpringBoot/src/main/java/com/imlewis/tool/AssessmentTool.java
@@ -19,19 +19,17 @@ public class AssessmentTool {
 	}
 
 	public static String getHexColor(String colorString) {
-		StringBuffer colorResponse = new StringBuffer(COLOR_APPENDER);
+		String priceLabel = NOT_A_VALID_COLOR;
 		try {
 			Color color = ColorFactory.valueOf(colorString.toLowerCase());
-			colorResponse.append(getRGBCoodinates(color.getRed()));
-			colorResponse.append(getRGBCoodinates(color.getGreen()));
-			colorResponse.append(getRGBCoodinates(color.getBlue()));
+			priceLabel = String.join(StringUtils.EMPTY, COLOR_APPENDER, getRGBCoodinates(color.getRed()),
+					getRGBCoodinates(color.getGreen()), getRGBCoodinates(color.getBlue()));
 		} catch (IllegalArgumentException illegalArgumentException) {
-			colorResponse = new StringBuffer(NOT_A_VALID_COLOR);
 		}
-		return colorResponse.toString();
+		return priceLabel;
 	}
 
-	public static String getRGBCoodinates(int intColorValue) {
+	private static String getRGBCoodinates(int intColorValue) {
 		return Integer.toHexString(intColorValue).length() < 2 ? HEX_COLOR_APPENDER + Integer.toHexString(intColorValue)
 				: Integer.toHexString(intColorValue);
 	}
@@ -39,6 +37,6 @@ public class AssessmentTool {
 	public static String getFormattedPrice(String price) {
 		float floatPrice = getFloatValue(price);
 		return floatPrice < 10 ? GBP_CURRENCY + String.format(TWO_DECIMAL_POINTS, floatPrice)
-				: GBP_CURRENCY  + String.format(NO_DECIMAL_POINTS, floatPrice);
+				: GBP_CURRENCY + String.format(NO_DECIMAL_POINTS, floatPrice);
 	}
 }
